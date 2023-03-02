84f9df0884c21b80e4302796dae392416005a6da
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-03-02 12:41:39 +0100

    feat:netflixclone

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import Navbar from "./componenets/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
