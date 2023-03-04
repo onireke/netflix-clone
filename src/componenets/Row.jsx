341d703f1020061deed9f0efdcfe15d9e93532de
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-03-04 12:17:20 +0100

    feat:netflixclone

--- src/componenets/Row.jsx
+++ src/componenets/Row.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "../styles/componentsStyles/Row.scss";
 
 function Row({ title, fetchURL }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+
+  console.log(movies);
   return (
     <>
       <h2 className="movie-titles">{title}</h2>
+      <div className="movies-list">
+        {movies.map((item, id) => (
+          <div></div>
+        ))}
+      </div>
     </>
   );
 }
