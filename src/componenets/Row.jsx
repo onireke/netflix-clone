0be2aadc0d8e1d2da14bfada7c941c79e9b09dbd
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-03-04 11:51:12 +0100

    feat:netflixclone

--- src/componenets/Row.jsx
+++ src/componenets/Row.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../styles/componentsStyles/Row.scss";
 
-function Row() {
-  return <div></div>;
+function Row({ title, fetchURL }) {
+  return (
+    <>
+      <h2 className="movie-titles">{title}</h2>
+    </>
+  );
 }
 
 export default Row;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import HomeMain from "../componenets/HomeMain";
+import Row from "../componenets/Row";
+import requests from "../Requests";
 
 function Home() {
   return (
     <div>
       <HomeMain />
+      <Row title="Popular" fetchURL={requests.requestPopular} />
+      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row title="Trending" fetchURL={requests.requestTrending} />
+      <Row title="Up Coming" fetchURL={requests.requestUpcoming} />
+      <Row title="Latest" fetchURL={requests.requestLatest} />
     </div>
   );
 }
