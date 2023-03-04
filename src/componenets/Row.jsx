fdab6422dc5e9294fdb5024eaa92765049c424d4
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-03-04 18:34:59 +0100

    feat:netflixclone

--- src/componenets/Row.jsx
+++ src/componenets/Row.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "../styles/componentsStyles/Row.scss";
-
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
+  const [like, setlike] = useState(false);
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -17,14 +18,15 @@ function Row({ title, fetchURL }) {
       <h2 className="movies-title">{title}</h2>
       <div id={"slider"}>
         {movies?.map((item, id) => (
-          <div className="movies-list">
+          <div className="movies-list relative">
             <img
               src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
               alt={item?.title}
             />
 
-            <div className="overly">
+            <div className="overly absolute">
               <p className="overly-title">{item?.title}</p>
+              <p className="like">{like ? <FaHeart /> : <FaRegHeart />}</p>
             </div>
           </div>
         ))}
