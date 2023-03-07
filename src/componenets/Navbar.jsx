5f3643725f53d030e4826b7be659bd5ce0c79608
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-03-07 12:49:34 +0100

    feat: context

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "../styles/componentsStyles/Navbar.scss";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <main className="navbar">
-      <h1 className="netflix">NETFLIX</h1>
+      <Link to="/">
+        {" "}
+        <h1 className="netflix">NETFLIX</h1>
+      </Link>
       <div className="buttons">
-        <button className="sign-in">Sign in</button>
-        <button className="sign-up">Sign up</button>
+        <Link to="/login">
+          {" "}
+          <button className="sign-in">Sign in</button>
+        </Link>
+        <Link to="/ signup">
+          {" "}
+          <button className="sign-up">Sign up</button>
+        </Link>
       </div>
     </main>
   );
