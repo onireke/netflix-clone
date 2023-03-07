60a6d92e98a5ba5c95c317fc4adcce325565406c
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-03-07 12:15:28 +0100

    feat: context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+import { auth } from "../firebase";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updateCurrentUser,
+} from "firebase/auth";
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState();
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function logIn(email, password) {
+    return signInWithEmailAndPassword(auth, password, email);
+  }
+
+  function logOut() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      return setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  });
+  return (
+    <AuthContext.Provider value={{ user, logIn, logOut, signUp }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function UserAuth() {
+  return useContext(AuthContext);
+}
